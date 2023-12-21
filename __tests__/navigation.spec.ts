fa1224184d735b3fde5cc469ba87dd82eafccdbb
Alex <alexbor04gv@gmail.com>
2023-12-21 18:45:20 +0400

    add gh actions

--- __tests__/navigation.spec.ts
+++ __tests__/navigation.spec.ts
@@ -1,19 +1,30 @@
 import { test } from '@playwright/test';
 
-test('navigation & theme', async ({ page }) => {
+test('test', async ({ page }) => {
   await page.goto('/');
+  await page.goto('/ru');
+  await page.getByRole('button', { name: 'Toggle theme' }).click();
+  await page.getByRole('menuitem', { name: 'Light' }).click();
+  await page.getByRole('button', { name: 'Toggle theme' }).click();
+  await page.getByRole('menuitem', { name: 'System' }).click();
+  await page.getByRole('button', { name: 'Toggle theme' }).click();
+  await page.getByRole('menuitem', { name: 'Dark' }).click();
+  await page.getByRole('button', { name: 'Toggle theme' }).click();
+  await page.getByRole('menuitem', { name: 'Light' }).click();
   await page.getByRole('link', { name: 'Russian Pathnames' }).click();
-  await page.getByRole('link', { name: 'Главная страница' }).click();
-  await page.getByRole('button', { name: 'Change language' }).click();
-  await page.getByRole('menuitem', { name: 'RU' }).click();
   await page.getByRole('button', { name: 'Change language' }).click();
   await page.getByRole('menuitem', { name: 'EN' }).click();
-  await page.getByRole('link', { name: 'Pathnames' }).click();
-  await page.getByRole('link', { name: 'Home' }).click();
+  await page.getByRole('button', { name: 'Change language' }).click();
+  await page.getByRole('menuitem', { name: 'RU' }).click();
+  await page.getByRole('link', { name: 'Главная страница' }).click();
   await page.getByRole('button', { name: 'Toggle theme' }).click();
   await page.getByRole('menuitem', { name: 'Light' }).click();
   await page.getByRole('button', { name: 'Toggle theme' }).click();
   await page.getByRole('menuitem', { name: 'Dark' }).click();
-  await page.getByRole('button', { name: 'Toggle theme' }).click();
-  await page.getByRole('menuitem', { name: 'System' }).click();
+  await page.getByRole('link', { name: 'Russian Pathnames' }).click();
+  await page.getByRole('link', { name: 'Главная страница' }).click();
+  await page.getByRole('link', { name: 'Russian Pathnames' }).click();
+  await page.getByRole('link', { name: 'Главная страница' }).click();
+  await page.getByRole('button', { name: 'Change language' }).click();
+  await page.getByRole('menuitem', { name: 'EN' }).click();
 });
